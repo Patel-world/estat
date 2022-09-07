45a0f298413c9645d0fbd40b3a01553703332d10
Pankaj Kumar Verma <patel.dp2024@gmail.com>
2022-09-07 23:43:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import Category from './components/Category';
+import Search from './components/Search';
+import Listing from './components/Listing';
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <div className='nheader'>
+        <div className='flexy'><h1>Search Properties to Rent</h1></div>
+        <Search/>
       </div>
-    )
-  }
+      <Category/>
+      <Listing/>
+    </div>
+  );
 }
 
-export default App
+export default App;
